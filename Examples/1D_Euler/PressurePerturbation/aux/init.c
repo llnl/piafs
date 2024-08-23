e037b66a0815b7f0a1262a4cde917168adc461e1
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2024-08-22 18:22:31 -0700

    added the 1D pressure perturbation equation and corrected README for other examples

---
+++ Examples/1D_Euler/PressurePerturbation/aux/init.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main()
+{
+  // default values
+  int NI=101;
+  int ndims=1;
+  char ip_file_type[50]; strcpy(ip_file_type,"ascii");
+
+  // domain
+  double xmin = -50.0;
+  double xmax = 50.0;
+
+  // uniform flow parameters
+  double gamma = 1.4;
+  double rho0 = 1.0;
+  double p0 = 1.0;
+  double u0 = 0.0;
+
+  // pressure pertubation
+  double width = 5.0;
+  double ampl = 1.0;
+
+  FILE *in;
+  printf("Reading file \"solver.inp\"...\n");
+  in = fopen("solver.inp","r");
+  if (!in) {
+    printf("Error: Input file \"solver.inp\" not found. Default values will be used.\n");
+  } else {
+    char word[500];
+    fscanf(in,"%s",word);
+    if (!strcmp(word, "begin")){
+      while (strcmp(word, "end")){
+        fscanf(in,"%s",word);
+        if (!strcmp(word, "ndims"))     fscanf(in,"%d",&ndims);
+        else if (!strcmp(word, "size")) fscanf(in,"%d",&NI);
+        else if (!strcmp(word, "ip_file_type")) fscanf(in,"%s",ip_file_type);
+      }
+    } else printf("Error: Illegal format in solver.inp. Crash and burn!\n");
+  }
+  fclose(in);
+
+  if (ndims != 1) {
+    printf("ndims is not 1 in solver.inp. this code is to generate 1D initial conditions\n");
+    return(0);
+  }
+  printf("Grid:\t\t\t%d\n",NI);
+
+  int i;
+  double dx = (xmax - xmin) / ((double)(NI-1));
+
+  double *x, *rho,*rhou,*e;
+  x    = (double*) calloc (NI, sizeof(double));
+  rho  = (double*) calloc (NI, sizeof(double));
+  rhou = (double*) calloc (NI, sizeof(double));
+  e    = (double*) calloc (NI, sizeof(double));
+
+  for (i = 0; i < NI; i++){
+    x[i] = xmin + i*dx;
+    double sigma = width / (2.0 * sqrt(2.0*log(2.0)));
+    double dp = ampl*exp(-0.5*(x[i]/sigma)*(x[i]/sigma));
+    double pressure = p0 + dp;
+    double energy = pressure/(gamma-1.0) + 0.5*rho0*u0*u0;
+    rho[i]  = rho0;
+    rhou[i] = rho0 * u0;
+    e[i]    = energy;
+  }
+
+  if (!strcmp(ip_file_type,"ascii")) {
+    FILE *out;
+    out = fopen("initial.inp","w");
+    for (i = 0; i < NI; i++)  fprintf(out,"%lf ",x[i]);
+    fprintf(out,"\n");
+    for (i = 0; i < NI; i++)  fprintf(out,"%lf ",rho[i]);
+    fprintf(out,"\n");
+    for (i = 0; i < NI; i++)  fprintf(out,"%lf ",rhou[i]);
+    fprintf(out,"\n");
+    for (i = 0; i < NI; i++)  fprintf(out,"%lf ",e[i]);
+    fprintf(out,"\n");
+    fclose(out);
+  } else if ((!strcmp(ip_file_type,"binary")) || (!strcmp(ip_file_type,"bin"))) {
+    printf("Error: Writing binary initial solution file not implemented. ");
+    printf("Please choose ip_file_type in solver.inp as \"ascii\".\n");
+  }
+
+  free(x);
+  free(rho);
+  free(rhou);
+  free(e);
+
+  return(0);
+}
